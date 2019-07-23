4736835528b14c2808875550bcdc70cb5fe5aee2
wangke <zhwangke@gmail.com>
2019-07-23 21:04:25 +0800

    集群部署demo

---
+++ gossip-dubboxDemo-parent/dubboxdemo-service/src/main/java/com/dubboxdemo/service/UserService.java
@@ -0,0 +1,10 @@
+package com.dubboxdemo.service;
+
+/**
+ * @Author: WK
+ * @Data: 2019/7/21 0:43
+ * @Description: com.dubboxdemo.service
+ */
+public interface UserService {
+    public String getName();
+}
